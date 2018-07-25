93d09c7bb14babcb4a8492350ac8f573017f3b61
krishnabose02 <krishna.bose02@gmail.com>
2018-07-25 19:05:31 +0530

    car add page

--- src/pages/add-cars/add-cars.ts
+++ src/pages/add-cars/add-cars.ts
@@ -17,6 +17,7 @@ import { ImagePicker } from '@ionic-native/image-picker';
 })
 export class AddCarsPage {
 
+
   public token: string = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjA1MWNkNzUyZjhjZWUzY2Q0MmQzNzAzY2ExOGZkZmU4NDc0NmJlMzBiNzI3ZmM2ZWNkZTYyN2ExNWNmMzI5ODRjYjA2NzY2YjI2ZTk2YzE0In0.eyJhdWQiOiIxIiwianRpIjoiMDUxY2Q3NTJmOGNlZTNjZDQyZDM3MDNjYTE4ZmRmZTg0NzQ2YmUzMGI3MjdmYzZlY2RlNjI3YTE1Y2YzMjk4NGNiMDY3NjZiMjZlOTZjMTQiLCJpYXQiOjE1MzE5ODY1OTEsIm5iZiI6MTUzMTk4NjU5MSwiZXhwIjoxNTYzNTIyNTkxLCJzdWIiOiIzIiwic2NvcGVzIjpbXX0.AqYVcs-YCspA2pyteHvB9jsh18aX8qaJzmlq9oUuXb0PJ_zUwj272itEKscJ4LhMlApt9GhrCtdTEK-90MVMbDVXp1z7kj_Mg9wzULyXVpdj9xQUwkRZsLcINLqpHgrsleZcUuNcuwKiibVLMjRcJ5Jz_eOnbr4E62UxQVUkOjVzEciAjucdI73FepK5HjOaEmW4qjxuxtbO8zeVotQn42yWDsTtRqL8-GNBn4MT2lQuMMS9m89SioVD4WHbSeAURMd1gQbiju1xSqmzUc7rDnNUniIhVQYkmmm39zbCJ2hQlfvTC1CCWz6i-h18AXbKvdSqauRlBIodHklV3mvgqyhkKHFn7EOCf8FUEpTJrwMJVGKuEa8iuCMJnYe7dBvAdqXp-CjgcoD2jiCU6GhilmzTmP2Ec2g-K18Wu4t2aPZlTOoFUpQd6P_P3m7kFn7Dv8Z_UKrJk-mLZA1KV5EfX-QYn95pM0sB3Pqjg-6xllFa8qk9ZdSeDRY8ZN7-23ye1J5-v2GQ3b0m5Mm8cqN0WGzVyma5PkIah7ioBgFN9co6QOdDh_SfHdN_Y74bQdzp2LI97h02O0YQQRYu7z_eBD1FEgzxMzj2CSOs3Z9zl3noLjd1T8mF6A-3qx89dPBZ84WE5SpTaLrSXh7V4hVeLyXZbuBnZ3C7g2ogvt0-W3I";
   @ViewChild('formslides') formSlide: Slides;
   @ViewChild('sliderbubbles') sliderbubbles: Slides;
@@ -35,6 +36,7 @@ export class AddCarsPage {
 
   //form 2 data
   public ac_available: boolean = false;
+  public form2data: {no_of_seats:number, min_hire_period: number, max_hire_period: number, car_price_hour: number, car_price_kil: number, ac_car_price_hour: number, ac_car_price_kil: number, book_advance: number, car_tags: string};
 
   //form 3 data
   public images: string[];
@@ -50,7 +52,7 @@ export class AddCarsPage {
               public imagePicker: ImagePicker
   ) {
     //for card slide design
-    this.pageNo = 0;
+    this.pageNo = 2;
     this.len = 1;
 
 
@@ -62,6 +64,7 @@ export class AddCarsPage {
     this.initCarData();
 
     //for form 2
+    this.form2data ={no_of_seats:null, ac_car_price_hour:null, ac_car_price_kil:null, book_advance:null,car_price_hour:null,car_price_kil:null,car_tags:"",max_hire_period:null,min_hire_period:null};
 
     //for form 3
     //for dummies
@@ -86,6 +89,7 @@ export class AddCarsPage {
       this.slides.push({});
     }
 
+    this.formSlide.slideTo(2);
     this.formSlide.lockSwipes(true);
     this.sliderbubbles.lockSwipes(true);
   }
@@ -93,15 +97,20 @@ export class AddCarsPage {
   goToNext()
   {
     //TODO: validation suppresed due to rapid development, uncomment these
-    /*
+    
+    if(this.pageNo == 3)
+    {
+      this.uploadData();
+    }
     if(!this.validateStep(this.pageNo+1)) // step number is one more than pageNo, thanks to array base zero
     {
       console.log("unknown error "+ this.pageNo);
       return;
     }
-    */
+    
     //console.log(this.form1data);
     this.pageNo++;
+    this.errormessage = "";
     this.formSlide.lockSwipes(false);
     this.formSlide.slideNext();
     this.formSlide.lockSwipes(true);
@@ -150,7 +159,46 @@ export class AddCarsPage {
       this.errormessage = "";
       return true;
     }
-    return true;
+
+    if(stepNo == 2)
+    {
+      if(this.isInvalid(this.form2data.no_of_seats, "Enter valid no. of seats"))
+      return false;
+      else if(this.isInvalid(this.form2data.min_hire_period, "Enter valid minimum period of hire"))
+      return false;
+      else if(this.form2data.max_hire_period<this.form2data.min_hire_period || this.form2data.max_hire_period == null)
+      {
+        this.errormessage = "Enter valid maximum hiring period";
+        return false;
+      }
+      else if(this.isInvalid(this.form2data.car_price_hour, "Enter valid rate per hour (Non-AC)"))
+      return false;
+
+      else if(this.isInvalid(this.form2data.car_price_kil, "Enter valid rate per KM (Non-AC)"))
+      return false;
+
+      else if(this.ac_available)
+      {
+        if(this.isInvalid(this.form2data.ac_car_price_hour, "Enter valid rate per hour (AC)"))
+        return false;
+
+        if(this.isInvalid(this.form2data.ac_car_price_kil, "Enter valid rate per KM (AC)"))
+        return false;
+      }
+
+      else if(this.isInvalid(this.form2data.book_advance, "Enter a valid advance booking fee"))
+      return false;
+
+      else if(this.form2data.car_tags.trim() == "")
+      {
+        this.errormessage = "Enter some tags to identify your product";
+        return false;
+      }
+      return true;
+
+    }
+
+    return false;
   }
 
   //functions for form 1
@@ -186,7 +234,7 @@ export class AddCarsPage {
   {
     //TODO: change the token to load from local storage
     console.log(brand);
-    this.form1data.brand = brand.car_company_name;
+    this.form1data.brand = brand.id+"";
     this.form1data.model = null;
     this.errormessage = "";
     this.restServ.getData("get_car_models?id="+brand.id, this.token).then((result)=>
@@ -209,7 +257,7 @@ export class AddCarsPage {
 
   storeModel(model: {car_model:string, model_id:number})
   {
-    this.form1data.model = model.car_model;
+    this.form1data.model = model.model_id+"";
     this.errormessage = "";
   }
 
@@ -224,11 +272,25 @@ export class AddCarsPage {
   }
 
   //functions for form 2
+  isInvalid(value: number, errorm: string):boolean
+  {
+    if(value<=0 || value == null)
+    {
+      this.errormessage = errorm;
+      return true;
+    }
+    return false;
+  }
 
+  clearAcRates()
+  {
+    this.form2data.ac_car_price_hour = 0;
+    this.form2data.ac_car_price_kil = 0;
+  }
   //function for form 3
   pickImage()
   {
-    this.imagePicker.getPictures({maximumImagesCount:2, quality:10}).then
+    this.imagePicker.getPictures({maximumImagesCount:2, quality:10, outputType:1}).then
     (results =>{
       console.log(results);
       for(let i=0; i < results.length;i++){
@@ -245,5 +307,39 @@ export class AddCarsPage {
         newimage.push(element);
     });
     this.images = newimage;
+  }
+
+  uploadData()
+  {
+    //call rest service and upload the carData
+    let carData: any;
+    if(this.unknowncar)
+    {
+      carData.brand = this.form1data.brand; 
+      carData.model = this.form1data.model;
+    }
+    else{
+      carData.brand_id = this.form1data.brand;
+      carData.model_id = this.form1data.model;
+    }
+    //carData.business_id = XXXXXX  Probably available in localstorage
+    carData.car_tags = this.form2data.car_tags;
+    carData.car_type = this.form1data.type;
+    carData.car_price_hour = this.form2data.car_price_hour;
+    carData.car_price_kil = this.form2data.car_price_kil;
+    carData.min_hire_period = this.form2data.min_hire_period;
+    carData.max_hire_period = this.form2data.max_hire_period;
+    if(this.ac_available)
+    {
+      carData.ac_car_price_hour = this.form2data.ac_car_price_hour;
+      carData.ac_car_price_kil = this.form2data.ac_car_price_kil;
+    }
+    carData.book_advance = this.form2data.book_advance;
+    carData.images = [""];
+    this.images.forEach(element => {
+      carData.images.push(element);
+    });
+
+    //call the rest here..
   }
 }
