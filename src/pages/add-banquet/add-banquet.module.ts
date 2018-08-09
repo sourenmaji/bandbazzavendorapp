a78e857309acc376f223456052a88869adefee12
Suchismita Basu <suchismita.basu17@gmail.com>
2018-08-09 15:49:17 +0530

    Splash screen and icon change

--- src/pages/add-banquet/add-banquet.module.ts
+++ src/pages/add-banquet/add-banquet.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AddBanquetPage } from './add-banquet';
+import { AgmCoreModule } from '../../../node_modules/@agm/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,10 @@ import { AddBanquetPage } from './add-banquet';
   ],
   imports: [
     IonicPageModule.forChild(AddBanquetPage),
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyDDK5MydVx-HkNyQcPTBdDyIyrqbwVPST0",
+      libraries: ["places"]
+  })
   ],
 })
 export class AddBanquetPageModule {}
